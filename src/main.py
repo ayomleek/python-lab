d43baf826477326839e1561ed03853575fc5e647
ayomleek <ayomleek@gmail.com>
2026-08-23 13:15:12 +0300

    Add utility functions and user input program

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from utils import square, is_even, celsius_to_fahrenheit
+
+
+number = float(input("Enter a number: "))
+
+print(f"The square of {number} is {square(number)}.")
+
+if is_even(number):
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
+
+print(f"{number}°C is equal to {celsius_to_fahrenheit(number)}°F.")
